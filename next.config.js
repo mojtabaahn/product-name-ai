ccbc2b4e83894235e643e5dbd0f95c927df127c4
jozi <hosein.jozi@gmail.com>
2024-11-24 15:19:14 +0330

    liara config env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    HELICONE_API_KEY: process.env.HELICONE_API_KEY,
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
